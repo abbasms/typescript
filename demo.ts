5dcd9ac27022c7bf61201624c655149bad502184
Abbas [Work] <abbas.sherawala@hp.com>
2015-05-29 17:41:02 -0400

    More union and guard examples

--- demo.ts
+++ demo.ts
@@ -1,3 +1,18 @@
+
+//Unions and TypeGuards
+
+function listNames(names: string | string[]) {
+	if (typeof names == "string")
+		console.log(names);
+	else
+		for (name of names)
+			listNames(name);
+}
+
+listNames("Vegeta");
+listNames(["Goku", "Gohan", "Piccollo"]);
+
+
 class Saiyan {
 	name: string;
 	powerLevel: number;
@@ -28,26 +43,33 @@ class Human {
 
 
 function saiyanTest(sentient: Human|Saiyan) {
+	//Properties common to Saiyan and human accessible here
+	console.log(sentient.name);
 	if (sentient instanceof Saiyan) {
+		//Properties of Saiyan accessible here
 		sentient.capturePlanets();
 	}
 	else if (sentient instanceof Human) {
+		//Properties of Human accessible here
 		sentient.doNothing();
 	}
 }
 
-saiyanTest(new Human("Son", "Goku"));
+saiyanTest(new Human("Goku", "Warrior"));
+
+
+//Type Inference
+//Array<string|number>
+
+var thisIsAVeryBadVariableName = ['Goku', 34];
+//var thisIsAVeryBadVariableName = ['Goku', 34, false];
+thisIsAVeryBadVariableName[0] = false;
 
 
 
+//Type Aliases
 
-function listNames<T>(names: string | string[]) {
-	if (typeof names == "string")
-		console.log(names);        
-	else
-		for (name of names)
-			listNames(name);
-}
+//type NgScope = ng.IScope;
+type Callback = () => {};
+type MixedArray = Array<string|number|boolean>
 
-listNames("Saiyan");
-listNames("James");
